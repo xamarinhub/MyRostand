76e4c9edffb4998f26b70b88651293c08a81c26d
AlexisL2607 <lerouxalexispro@gmail.com>
2020-01-28 14:37:20 +0100

    Modification de la présentation de la page de connexion

--- MyRostand/MyRostand/login.xaml.cs
+++ MyRostand/MyRostand/login.xaml.cs
@@ -21,8 +21,50 @@ namespace MyRostand
             {
                 Margin = new Thickness(30,20,30,0)
             };
+            Image LogoAccueil = new Image()
+            {
+                Source = "myrostandaccueil.jpg"
+            };
 
             StackLayout stackForm = new StackLayout();
+
+            Frame frameInformation = new Frame()
+            {
+                CornerRadius = 10,
+                BorderColor = Color.FromHex("#27ae60"),
+                Margin = new Thickness(100, 20, 100, 20),
+                Padding = new Thickness(0, 10, 0, 10),
+            };
+            StackLayout titreInformation = new StackLayout()
+            {
+                HorizontalOptions = LayoutOptions.Fill,
+                BackgroundColor = Color.FromHex("#27ae60"),
+                HeightRequest = 35,
+            };
+            Label titre2 = new Label()
+            {
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalTextAlignment = TextAlignment.Center,
+                Text = "Informations",
+                FontSize = 20,
+                TextColor = Color.White
+            };
+            Label descriptionInformation = new Label()
+            {
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalTextAlignment = TextAlignment.Center,
+                Text = "Pour vous connecter, veuillez utiliser votre adresse mail et votre mot de passe fourni par l'établissement " + "\n" + " Exemple : prenom.nom@lyceejeanrostand.fr",
+                FontSize = 15,
+                BackgroundColor = Color.LightGray,
+                TextColor = Color.White
+            };
+
+            titreInformation.Children.Add(titre2);
+            stackForm.Children.Add(titreInformation);
+            stackForm.Children.Add(descriptionInformation);
+            frameInformation.Content = stackForm;
+
+
             Entry loginEntry = new Entry()
             {
                 Placeholder = "Identifiant"
@@ -38,6 +80,8 @@ namespace MyRostand
                 BackgroundColor = Color.FromHex("#27ae60"),
                 CornerRadius = 10,
                 Text = "Se connecter",
+                Margin = new Thickness(220, 10, 220, 10),
+                Padding = new Thickness(0, 10, 0, 10),
                 TextColor = Color.White
             };
 
@@ -46,6 +90,8 @@ namespace MyRostand
             stackForm.Children.Add(loginEntry);
             stackForm.Children.Add(passwordEntry);
             stackForm.Children.Add(connexion);
+            stackPrincipal.Children.Add(LogoAccueil);
+            stackPrincipal.Children.Add(frameInformation);
             stackPrincipal.Children.Add(stackForm);
 
             Content = stackPrincipal;
